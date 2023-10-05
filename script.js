cc035224a8f59f5ddc639699bbe6066f75ddc495
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2023-10-05 21:40:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+let arr=[
+   {
+    pic:"https://images.unsplash.com/photo-1518137020124-5b133ed2a118?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1629&q=80",
+    status:"https://images.unsplash.com/photo-1518137020124-5b133ed2a118?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1629&q=80"
+    }
+    ,
+   {
+    pic:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    status:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg",
+    status:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/311/74/429/scarlet-macaw-rainbow-parrot-plumage-red.jpg",
+    status:"https://p1.pxfuel.com/preview/951/283/638/parrot-close-lorikeet-trichoglossus-rainbow.jpg"
+    },
+    ,
+   {
+    pic:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    status:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg",
+    status:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/311/74/429/scarlet-macaw-rainbow-parrot-plumage-red.jpg",
+    status:"https://p1.pxfuel.com/preview/951/283/638/parrot-close-lorikeet-trichoglossus-rainbow.jpg"
+    },
+    ,
+   {
+    pic:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    status:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg",
+    status:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/311/74/429/scarlet-macaw-rainbow-parrot-plumage-red.jpg",
+    status:"https://p1.pxfuel.com/preview/951/283/638/parrot-close-lorikeet-trichoglossus-rainbow.jpg"
+    },
+    ,
+   {
+    pic:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    status:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg",
+    status:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/311/74/429/scarlet-macaw-rainbow-parrot-plumage-red.jpg",
+    status:"https://p1.pxfuel.com/preview/951/283/638/parrot-close-lorikeet-trichoglossus-rainbow.jpg"
+    },
+    ,
+   {
+    pic:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    status:"https://images.unsplash.com/photo-1476817343404-01ccd61218d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg",
+    status:"https://p1.pxfuel.com/preview/1023/416/847/portrait-child-in-wheat-field.jpg"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/311/74/429/scarlet-macaw-rainbow-parrot-plumage-red.jpg",
+    status:"https://p1.pxfuel.com/preview/951/283/638/parrot-close-lorikeet-trichoglossus-rainbow.jpg"
+    },
+   {
+    pic:"https://p1.pxfuel.com/preview/677/846/426/owl-bird-eyes-dark.jpg",
+    status:"https://p1.pxfuel.com/preview/877/494/403/owl-bird-eyes-eagle-owl.jpg"
+    }
+]
+
+
+let statusbox=document.querySelector('.statusbox');
+let statuspage=document.querySelector('.statusPage');
+let flag="";
+arr.forEach((v,i) => {
+    // console.log(v.pic,i);
+statusbox.innerHTML +=`<div  class="status">
+<img src="${v.pic}" alt="img" id="${i}">
+</div>`;
+});
+statusbox.addEventListener('click',(details)=>{
+    let value=arr[details.target.id].status;
+    statuspage.style.display='block';
+    console.log(value);
+    statuspage.style.backgroundImage=`url(${value})`;
+    setTimeout(() => {
+        statuspage.style.display='none';
+    }, 2000);
+   
+})
